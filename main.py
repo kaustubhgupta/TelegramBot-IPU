b03da42eac9e22ed7c6b7169eddf8f5c572c88a5
kaustubhgupta <kaustubhgupta1828@gmail.com>
2020-07-03 21:45:30 +0530

    da

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def respond():
 
 @app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
-    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    s = bot.setWebhook(URL)
     if s:
         return "webhook setup ok"
     else:
