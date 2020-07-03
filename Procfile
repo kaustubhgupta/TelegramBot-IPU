29af122f99c889c73393110bd3129e4b6f4a3f51
kaustubhgupta <kaustubhgupta1828@gmail.com>
2020-07-03 21:30:15 +0530

    new approch

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python app.py
+web: gunicorn main:app
---
+++ credentials.py
@@ -0,0 +1,3 @@
+bot_token = '1120612687:AAF5hrRzXfEqi1hBbBCK1L2Qg1s5Y37bVrU'
+bot_user_name = "ipuBOT"
+URL = ' https://iputelebot.herokuapp.com/'
