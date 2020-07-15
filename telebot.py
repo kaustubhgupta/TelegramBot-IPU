ea22ccbc476f6bbd0a70d5d88793b4d4f6f221e0
kaustubhgupta <kaustubhgupta1828@gmail.com>
2020-07-16 01:11:35 +0530

    auth key now safe

--- telebot.py
+++ telebot.py
@@ -1,12 +1,13 @@
 import requests
 import json
 import configparser as cfg
+import os
 
 
 class TelegramBot:
 
     def __init__(self, config):
-        self.token = self.readToken(config)
+        self.token = os.environ.get('authkey')
         self.base = "https://api.telegram.org/bot{}/".format(self.token)
 
     def get_updates(self, offset=None):
@@ -22,11 +23,6 @@ class TelegramBot:
         url = self.base + 'sendMessage?chat_id={}&text={}&parse_mode=Markdown'.format(chat_id, msg)
         if msg is not None:
             requests.get(url)
-
-    def readToken(self, config):
-        parser = cfg.ConfigParser()
-        parser.read(config)
-        return parser.get('creds', 'token')
 
     def send_action(self, chat_id):
         url = self.base + 'sendChatAction?chat_id={}&action=TYPING'.format(chat_id)
